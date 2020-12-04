d36ad702172de44ddbc4201e341ea4ea4088820f
Steve <stevepesce879@gmail.com>
2020-12-04 14:15:14 -0500

    finished POJO's closes #2

--- src/main/java/spesce/springframework/mypetclinic/model/Pet.java
+++ src/main/java/spesce/springframework/mypetclinic/model/Pet.java
@@ -1,5 +1,7 @@
 package spesce.springframework.mypetclinic.model;
 
+import java.time.LocalDate;
+
 /**
  * Created By Steve P. on 12/4/2020
  * original package: spesce.springframework.mypetclinic.model
@@ -7,4 +9,30 @@ package spesce.springframework.mypetclinic.model;
  */
 public class Pet {
     private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
--- src/main/java/spesce/springframework/mypetclinic/model/PetType.java
+++ src/main/java/spesce/springframework/mypetclinic/model/PetType.java
@@ -6,4 +6,14 @@ package spesce.springframework.mypetclinic.model;
  * ------------------------------------------------------------------------------------------
  */
 public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
