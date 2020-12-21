565c8243db70bd8593c72b1722f4c4885c340fe0
Steve <stevepesce879@gmail.com>
2020-12-21 16:22:04 -0500

    added index with controller. closes #8

---
+++ pet-clinic-web/src/main/java/spesce/springframework/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package spesce.springframework.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created By Steve P. on 12/21/2020
+ * original package: spesce.springframework.mypetclinic.controllers
+ * ------------------------------------------------------------------------------------------
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index.htmlm"})
+    public String index(){
+        return "index";
+    }
+
+}
